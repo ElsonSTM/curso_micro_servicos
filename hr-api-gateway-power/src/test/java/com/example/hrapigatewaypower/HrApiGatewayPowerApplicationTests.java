512c3db9271d61ba2add7e9c46672acebecce31d
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-11-22 10:47:26 -0300

    Project hr-api-gateway-power created

---
+++ hr-api-gateway-power/src/test/java/com/example/hrapigatewaypower/HrApiGatewayPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrapigatewaypower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
